2ed38c709acc3d2424c7a002bdfb3c197ccd92ef
lebrancconvas <lebrancconvas@gmail.com>
2026-02-21 03:57:23 +0700

    ✨feat: assign symbol to number.

---
+++ first/data/symbol.ts
@@ -0,0 +1,18 @@
+export const SYMBOL = {
+  "~": 1,
+  "^": 2,
+  "v": 3,
+  "c": 4,
+  "=": 5,
+  "0": 6,
+  "s": 7,
+  "(": 8,
+  ")": 9,
+  "E": 10,
+  "A": 11,
+  "+": 12,
+  "*": 13
+};
+
+
+export type Symbolic = keyof typeof SYMBOL; 
